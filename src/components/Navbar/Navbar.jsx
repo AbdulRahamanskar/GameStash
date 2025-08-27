11db11d26821548a43cee2b1f2a8f6386680d785
Shaik Abdul Rahaman <162731656+AbdulRahamanskar@users.noreply.github.com>
2025-08-27 18:53:31 +0530

    recent changes 3

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       <div
         className="max-w-screen-2xl container mx-auto
     px-4 md:px-20 h-16 shadow-md 
-    fixed top-0 right-0 left-0 bottom-0 z-50 bg-white">
+    fixed top-0 right-0 left-0 z-50 bg-white">
         <div className="flex justify-between items-center h-16">
           <div className="flex space-x-2">
             <img
